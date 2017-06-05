1c582c77046f87ad3c34da03d24074a896fc049a
leeheekyo <lhk5895@gmail.com>
2017-06-05 22:27:59 +0900

    Android library program with android studio and php server

---
+++ app/src/main/java/com/example/samsung/pnulibrary5/StudyRoomReservation.java
@@ -0,0 +1,478 @@
+package com.example.samsung.pnulibrary5;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AbsoluteLayout;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class StudyRoomReservation extends ActionBarActivity {
+
+    private Button move;
+    private TextView[] days = new TextView[4];
+    private TextView title_l;
+
+    private final int buttonCountMax = 4;
+    private final int tCountMax = 3;
+    private final int trCountMax=3;
+    private final int timeCountMax = 15;
+
+
+    private int daySelect;
+    private String intentText =  "";
+    private String myStudentID="201224521";
+
+    TableRow[][] trs = new TableRow[tCountMax][trCountMax];
+    TextView[][] tvs = new TextView[tCountMax][timeCountMax];
+
+    private StringBuilder sb = new StringBuilder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_room_reservation);
+
+        move = (Button)findViewById(R.id.move);
+        days[0] = (TextView)findViewById(R.id.today);
+        days[1] = (TextView)findViewById(R.id.oneday);
+        days[2] = (TextView)findViewById(R.id.twoday);
+        days[3] = (TextView)findViewById(R.id.threeday);
+        title_l = (TextView)findViewById(R.id.title_l);
+
+        /*try {
+            StringBuilder sb = new StringBuilder();
+            URL url = new URL("http://164.125.35.32/temp/kyo/studyroomcheck.php");
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();// 접속
+            if (conn != null) {
+                conn.setConnectTimeout(2000);
+                conn.setUseCaches(false);
+                if (conn.getResponseCode()==HttpURLConnection.HTTP_OK){
+                    //    데이터 읽기
+                    BufferedReader br= new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
+                    String line = br.readLine();
+                    sb.append(line);
+                    br.close(); // 스트림 해제
+                }
+                conn.disconnect(); // 연결 끊기
+            }
+            // 값을 출력하기
+            Log.d("testCrawl", sb.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d("testCrawl", "error : " +e.toString());
+        }*/
+
+        int size ;
+
+
+
+        //date check
+        long now = System.currentTimeMillis();
+        Date date = new Date(now);
+
+        SimpleDateFormat CurDataFormat = new SimpleDateFormat("MM-dd");
+
+        int i;
+        days[0].setText(CurDataFormat.format(date));
+        String tstring  = days[0].getText().toString();
+        daySelect = Integer.valueOf(tstring.substring(0,2)+tstring.substring(3));
+        for(i=1;i<buttonCountMax;i++){
+            date.setTime(date.getTime()+1000*24*60*60);
+            days[i].setText(CurDataFormat.format(date));
+        }
+        for(i=0;i<buttonCountMax;i++){
+            days[i].setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    for(int i=0; i<buttonCountMax; i++){
+                        if(v.equals(days[i])){
+                            String tstring  = days[i].getText().toString();
+                            daySelect = Integer.valueOf(tstring.substring(0,2)+tstring.substring(3));
+                        }
+                        days[i].setBackgroundColor(Color.LTGRAY);
+                    }
+                    //daySelect = ((Button)v).getText().toString();
+
+                    v.setBackgroundColor(Color.BLUE);
+                    repaint();
+                }
+            });
+        }
+
+
+        //Toast.makeText(this, "check",Toast.LENGTH_SHORT).show();
+
+        move.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //Intent Example
+                Intent intent = new Intent(StudyRoomReservation.this, StudyRoomReservationResult.class); // 두번째 액티비티를 실행하기 위한 인텐트
+                intent.putExtra("Text", intentText);
+                startActivityForResult(intent, 0);
+
+            }
+        });
+
+        //start table ui ***************************
+
+        // ActionBar.LayoutParams layoutParams = new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT, 1);
+
+        //tv.setHeight(ActionBar.LayoutParams.WRAP_CONTENT);
+        //tv.setLayoutParams(layoutParams);
+
+        //ImageView iv = new ImageView(this);
+        //iv.setImageResource(R.drawable.close);
+
+        //rl.addView(iv);
+
+
+        int trCount = 0;
+        int tCount =0;
+        int timeCount = 0;
+
+
+        size = title_l.getWidth()/5;
+
+        TableLayout[] tables = new TableLayout[3];
+        tables[0] = (TableLayout)findViewById(R.id.l241);
+        tables[1] = (TableLayout)findViewById(R.id.l242);
+        tables[2] = (TableLayout)findViewById(R.id.l243);
+
+
+        for(tCount=0;tCount<tCountMax;tCount++) {
+            for (timeCount = 0, trCount=0; timeCount < timeCountMax; timeCount++) {
+                if (timeCount % 5 == 0) {
+                    trs[tCount][trCount] = new TableRow(this);
+                    //trs[tCount][trCount].setPaddingRelative(1, 1, 1, 1);
+                    trs[tCount][trCount].setBackgroundColor(Color.BLACK);
+                }
+
+                tvs[tCount][timeCount] = new TextView(this);
+                if(timeCount ==0 || timeCount > 12){
+                    tvs[tCount][timeCount].setBackgroundColor(Color.BLACK);
+                }
+                else {
+                    if (timeCount < 2) tvs[tCount][timeCount].setText("0" + String.valueOf(timeCount + 8) + ":00");
+                    else tvs[tCount][timeCount].setText(String.valueOf(timeCount + 8) + ":00");
+                    tvs[tCount][timeCount].setBackgroundColor(Color.LTGRAY);
+                    tvs[tCount][timeCount].setTextSize(14);
+                    tvs[tCount][timeCount].setWidth(size);
+                    tvs[tCount][timeCount].setGravity(Gravity.CENTER);
+                    tvs[tCount][timeCount].setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            //TextView b = (TextView) findViewById(v.getId());
+                            //if(b.getDrawingCacheBackgroundColor()==Color.LTGRAY) {
+                            v.setBackgroundColor(Color.GREEN);
+
+                            //find object
+                            boolean finish = false;
+                            int tCount = -1;
+                            int timeCount = -1;
+                            for(int i=0; i<tCountMax; i++){
+                                for(int j=0; j<timeCountMax; j++){
+                                    if(v.equals(tvs[i][j])){
+                                        finish=true;
+                                        tCount =i;
+                                        timeCount = j;
+                                        break;
+                                    }
+                                }
+                                if(finish) break;
+                            }
+
+                            if(tCount!=-1 && timeCount != -1) {
+                                Intent intent = new Intent(StudyRoomReservation.this, StudyRoomReservationResult.class); // 두번째 액티비티를 실행하기 위한 인텐트
+
+                                String result ="";
+                                if(tCount==0) result+="241.";
+                                else if(tCount==1) result+="242.";
+                                else if(tCount==2) result+="243.";
+
+                                result += String.valueOf(daySelect);
+                                result += String.valueOf(timeCount+8);
+
+                                intent.putExtra("Text", result); // 242.60514
+                                startActivityForResult(intent, 0);
+                            }
+                            //}
+                            //tvs[tCount][timeCount].setBackgroundColor(Color.BLUE);
+                        }
+                    });
+                }
+
+                tvs[tCount][timeCount].setId(tCount * 100 + timeCount);
+                tvs[tCount][timeCount].setPadding(0, 25, 0, 25);
+
+
+
+                trs[tCount][trCount].addView(tvs[tCount][timeCount]);
+
+                if (timeCount % 5 == 4) {
+                    tables[tCount].addView(trs[tCount][trCount]);
+                    trCount++;
+                }
+            }
+        }
+
+
+
+        // start parsing ************************
+
+        //Toast.makeText(StudyRoomReservation.this, sb.toString(), Toast.LENGTH_SHORT).show();
+        //title_l.setText(sb.toString());
+
+        repaint();
+
+
+        //loadHtml("http://164.125.35.32/temp/kyo/studyroomcheck.php");
+
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 0 && resultCode == 0){
+            Toast.makeText(this, "ok",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    void loadHtml(final String urladdress) { // 웹에서 html 읽어오기
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    URL url = new URL(urladdress);
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();// 접속
+                    if (conn != null) {
+                        conn.setConnectTimeout(2000);
+                        conn.setUseCaches(false);
+                        if (conn.getResponseCode()==HttpURLConnection.HTTP_OK){
+                            //    데이터 읽기
+                            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
+                            while(true) {
+                                String line = br.readLine();
+                                if (line == null) break;
+                                sb.append(line+"\n");
+                            }
+                            br.close(); // 스트림 해제
+                        }
+                        conn.disconnect(); // 연결 끊기
+                    }
+                    // 값을 출력하기
+                    Log.d("testCrawl", sb.toString());
+                } catch (Exception e) {
+                    sb.append("error");
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start(); // 쓰레드 시작
+    }
+
+    public void repaint(){
+        int tCount;
+        int timeCount ;
+        for(tCount=0;tCount<tCountMax;tCount++) {
+            for (timeCount = 0; timeCount < timeCountMax; timeCount++) {
+                if(timeCount ==0 || timeCount > 12){
+                    tvs[tCount][timeCount].setBackgroundColor(Color.BLACK);
+                }
+                else {
+                    tvs[tCount][timeCount].setBackgroundColor(Color.LTGRAY);
+//                    tvs[tCount][timeCount].setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            v.setBackgroundColor(Color.BLUE);
+//                        }
+//                    });
+                }
+            }
+        }
+
+        //!!
+        /*
+http://164.125.35.32/temp/kyo/studyroomreservation.php?studyRoomNumber=242&studyRoomReservationTime=170614&user1=201224501&user2=201224502&user3=201224503&user4=201224504&user5=201224505
+
+http://164.125.35.32/temp/kyo/studyroomcheckid.php?id=201224501
+
+http://164.125.35.32/temp/kyo/studyroomcheck.php
+
+http://164.125.35.32/temp/kyo/idcheck.php?id=201224521
+ */
+        loadHtml("http://164.125.35.32/temp/kyo/studyroomcheck.php");
+        while(sb.length()==0) ;
+
+        String sbstring = sb.toString();
+        String[] sbstrings = sbstring.split("/");
+        int c = sbstrings.length-1;
+        int i;
+        for(i=0; i<c; i++) {
+            //Log.i("testString1",daySelect+" "+sbstrings[i].substring(4, sbstrings[i].length() - 2));
+
+            if (daySelect==Integer.valueOf(sbstrings[i].substring(4, sbstrings[i].length() - 2))) {
+                // 242.60516
+                //Log.i("teststring2", sbstrings[i].substring(0, 3) + sbstrings[i].substring(4));
+
+                int tCountone = -1;
+                if (sbstrings[i].substring(0, 3).equals("241")) {
+                    tCountone = 0;
+                } else if (sbstrings[i].substring(0, 3).equals("242")) {
+                    tCountone = 1;
+                } else if (sbstrings[i].substring(0, 3).equals("243")) {
+                    tCountone = 2;
+                }
+                //Log.i("testnumber", i + " " + tCountone);
+
+
+                // 603 604 605 // 60516
+                int buttonDate = Integer.valueOf(sbstrings[i].substring(4));
+
+                if (buttonDate / 100 == daySelect) {
+                    if (tCountone != -1) {
+                        tvs[tCountone][(buttonDate % 100) - 8].setBackgroundColor(Color.DKGRAY);
+                        tvs[tCountone][(buttonDate % 100) - 8].setOnClickListener(null);
+                    }
+                }
+
+                //Log.i("teststring3", buttonDate / 100 + " " + daySelect);
+            }
+        }
+        sb = new StringBuilder();
+
+        loadHtml("http://164.125.35.32/temp/kyo/studyroomcheckid.php?id="+myStudentID);
+        Log.i("StringCHeckId","http://164.125.35.32/temp/kyo/studyroomcheckid.php?id="+myStudentID);
+        while(sb.length()==0) ;
+
+        sbstring = sb.toString();
+        sbstrings = sbstring.split("/");
+        c = sbstrings.length-1;
+        for(i=0; i<c; i++) {
+            //Log.i("testString1",daySelect+" "+sbstrings[i].substring(4, sbstrings[i].length() - 2));
+
+            if (daySelect==Integer.valueOf(sbstrings[i].substring(4, sbstrings[i].length() - 2))) {
+                // 242.60516
+                //Log.i("teststring2", sbstrings[i].substring(0, 3) + sbstrings[i].substring(4));
+
+                int tCountone = -1;
+                if (sbstrings[i].substring(0, 3).equals("241")) {
+                    tCountone = 0;
+                } else if (sbstrings[i].substring(0, 3).equals("242")) {
+                    tCountone = 1;
+                } else if (sbstrings[i].substring(0, 3).equals("243")) {
+                    tCountone = 2;
+                }
+                //Log.i("testnumber", i + " " + tCountone);
+
+
+                // 603 604 605 // 60516
+                int buttonDate = Integer.valueOf(sbstrings[i].substring(4));
+
+                if (buttonDate / 100 == daySelect) {
+                    if (tCountone != -1) {
+                        tvs[tCountone][(buttonDate % 100) - 8].setBackgroundColor(Color.RED);
+                        tvs[tCountone][(buttonDate % 100) - 8].setOnClickListener(null);
+                    }
+                }
+
+                //Log.i("teststring3", buttonDate / 100 + " " + daySelect);
+            }
+        }
+        sb = new StringBuilder();
+
+    }
+
+
+}
+/*
+http://164.125.35.32/temp/kyo/studyroomreservation.php?studyRoomNumber=242&studyRoomReservationTime=170614&user1=201224501&user2=201224502&user3=201224503&user4=201224504&user5=201224505
+
+http://164.125.35.32/temp/kyo/studyroomcheckid.php?id=201224501
+
+http://164.125.35.32/temp/kyo/studyroomcheck.php
+
+http://164.125.35.32/temp/kyo/idcheck.php?id=201224521
+ */
+
+/*
+        //242.060616/242.060814/
+        String sbstring = sb.toString();
+        String[] sbstrings = sbstring.split("/");
+
+        //Toast.makeText(StudyRoomReservation.this, sbstrings[0], Toast.LENGTH_SHORT).show();
+        //Log.i("teststring1",sbstrings[0] + "  " + sbstrings[1] + " " + sbstrings[2] + " " + sbstrings.length);
+
+        int c = sbstrings.length-1;
+        String[] sstrings;
+        for(i=0; i<c; i++){
+            // 242.60516
+            sstrings = sbstrings[i].split(".");
+            Log.i("teststring2", sbstrings[i].substring(0,3) + sbstrings[i].substring(4,9) );
+
+            int tCountone=-1;
+            if(sbstrings[i].substring(0,3).equals("241")){
+                tCountone=0;
+            } else if(sbstrings[i].substring(0,3).equals("242")){
+                tCountone=1;
+            } else if(sbstrings[i].substring(0,3).equals("243")){
+                tCountone=2;
+            }
+            Log.i("testnumber",i + " " + tCountone);
+
+
+            String[] dateString;
+            //for(int m = 0, todayDate=0; m<4; m++) {
+                dateString = daySelect.toString().split("-");
+                int todayDate = Integer.valueOf(dateString[0]) * 100 + Integer.valueOf(dateString[1]);
+                int buttonDate = Integer.valueOf(sbstrings[i].substring(4));
+
+                if(buttonDate/100==todayDate){
+                    if(tCountone!=-1){
+                        tvs[tCountone][(buttonDate%100)-8].setBackgroundColor(Color.RED);
+                    }
+                }
+
+                Log.i("teststring3", buttonDate/100 + " " + todayDate);
+            //}
+        }
+
+ */
---
+++ app/src/main/java/com/example/samsung/pnulibrary5/StudyRoomReservationResult.java
@@ -0,0 +1,199 @@
+package com.example.samsung.pnulibrary5;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by SAMSUNG on 2017-05-29.
+ */
+
+class StudyRoomReservationResult extends AppCompatActivity {
+
+    private Button btn;
+    private Button submit;
+    private TextView text;
+    private TextView title;
+
+    private EditText[] sIDs = new EditText[5];
+    private Button[] bIDs = new Button[5];
+
+    private String inputString;
+    private StringBuilder sb = new StringBuilder();
+
+    private final int SIZE =5;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_room_reservation_result);
+
+        text = (TextView)findViewById(R.id.text);
+        btn = (Button)findViewById(R.id.btn);
+        submit = (Button)findViewById(R.id.submit);
+        title = (TextView)findViewById(R.id.title);
+
+        sIDs[0] = (EditText)findViewById(R.id.sID1);
+        sIDs[1] = (EditText)findViewById(R.id.sID2);
+        sIDs[2] = (EditText)findViewById(R.id.sID3);
+        sIDs[3] = (EditText)findViewById(R.id.sID4);
+        sIDs[4] = (EditText)findViewById(R.id.sID5);
+
+        bIDs[0] = (Button)findViewById(R.id.bID1);
+        bIDs[1] = (Button)findViewById(R.id.bID2);
+        bIDs[2] = (Button)findViewById(R.id.bID3);
+        bIDs[3] = (Button)findViewById(R.id.bID4);
+        bIDs[4] = (Button)findViewById(R.id.bID5);
+
+        int i;
+        int size = title.getWidth()-50;
+        for(i=0; i<SIZE; i++){
+            sIDs[i].setWidth(size);
+        }
+        for(i=0;i<SIZE; i++){
+            bIDs[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String urladdString="";
+                    int identity =-1;
+                    for(int i=0; i<SIZE; i++){
+                        if(v.equals(bIDs[i])){
+                            identity=i;
+                            urladdString = sIDs[i].getText().toString();
+                            Log.i("urlString","http://164.125.35.32/temp/kyo/idcheck.php?id="+urladdString);
+                            loadHtml("http://164.125.35.32/temp/kyo/idcheck.php?id="+urladdString);
+                            break;
+                        }
+                    }
+                    while(sb.length()==0) ;
+
+                    String sbString = sb.toString();
+                    Log.i("resultid",sbString);
+                    Log.i("resultid2",String.valueOf(!sbString.equals("0\n")));
+                    if(!sbString.equals("0\n") && identity != -1){ //exsist id
+                        boolean check=true;
+                        for(int i=0;i<identity;i++){ // no duplication
+                            if( (sIDs[i].getText().toString()).equals(sIDs[identity].getText().toString()) ) check=false;
+                        }
+                        if(check){
+                            sIDs[identity].setFocusable(false);
+                            bIDs[identity].setText("OK");
+                            bIDs[identity].setBackgroundColor(Color.GREEN);
+                            bIDs[identity].setOnClickListener(null);
+                            if(identity==SIZE-1){
+                                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                                imm.hideSoftInputFromWindow(sIDs[identity].getWindowToken(), 0);
+
+                            }
+                        }
+                    }
+
+                    sb = new StringBuilder();
+
+                }
+            });
+        }
+
+        Intent intent = getIntent();
+        inputString = intent.getStringExtra("Text"); //241.60415
+        text.setText(inputString);
+        Log.i("testInputText",inputString);
+
+        btn.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent outIntent = getIntent();
+                outIntent.putExtra("Text", 0);
+                setResult(0, outIntent);
+                finish();
+            }
+
+        });
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //http://164.125.35.32/temp/kyo/studyroomreservation.php?studyRoomNumber=242&studyRoomReservationTime=170614&user1=201224501&user2=201224502&user3=201224503&user4=201224504&user5=201224505
+                Log.i("resultStringTest",inputString.substring(0,3)+" "+inputString.substring(4)+" "+sIDs[0].getText() + " "+ sIDs[1].getText()+" "+sIDs[2].getText()+" "+sIDs[3].getText()+ " "+sIDs[4].getText());
+
+                loadHtml("http://164.125.35.32/temp/kyo/studyroomreservation.php?studyRoomNumber="+inputString.substring(0,3)+"&studyRoomReservationTime="+inputString.substring(4)+"&user1="+sIDs[0].getText()+"&user2="+sIDs[1].getText()+"&user3="+sIDs[2].getText()+"&user4="+sIDs[3].getText()+"&user5="+sIDs[4].getText());
+                while(sb.length()==0) ;
+
+                String sbString = sb.toString();
+                Log.i("sbStringTest",sbString);
+                if(sbString.equals("0\n")){
+                    Toast.makeText(StudyRoomReservationResult.this,"Success",Toast.LENGTH_SHORT);
+                }
+                else if(sbString.equals("1\n")){
+                    Toast.makeText(StudyRoomReservationResult.this,"Cannot access to DB", Toast.LENGTH_SHORT);
+                }
+                else if(sbString.equals("2\n")){
+                    Toast.makeText(StudyRoomReservationResult.this,"Create Error",Toast.LENGTH_SHORT);
+                }
+
+                sb = new StringBuilder();
+            }
+        });
+
+    }
+
+    void loadHtml(final String urladdress) { // 웹에서 html 읽어오기
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    URL url = new URL(urladdress);
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();// 접속
+                    if (conn != null) {
+                        conn.setConnectTimeout(2000);
+                        conn.setUseCaches(false);
+                        if (conn.getResponseCode()==HttpURLConnection.HTTP_OK){
+                            //    데이터 읽기
+                            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));
+                            while(true) {
+                                String line = br.readLine();
+                                if (line == null) break;
+                                sb.append(line+"\n");
+                            }
+                            br.close(); // 스트림 해제
+                        }
+                        conn.disconnect(); // 연결 끊기
+                    }
+                    // 값을 출력하기
+                    Log.d("testCrawl", sb.toString());
+                } catch (Exception e) {
+                    sb.append("error");
+                    e.printStackTrace();
+                }
+            }
+        });
+        t.start(); // 쓰레드 시작
+    }
+}
+
+/*
+http://164.125.35.32/temp/kyo/studyroomreservation.php?studyRoomNumber=242&studyRoomReservationTime=170614&user1=201224501&user2=201224502&user3=201224503&user4=201224504&user5=201224505
+
+http://164.125.35.32/temp/kyo/studyroomcheckid.php?id=201224501
+
+http://164.125.35.32/temp/kyo/studyroomcheck.php
+
+http://164.125.35.32/temp/kyo/idcheck.php?id=201224521
+ */
